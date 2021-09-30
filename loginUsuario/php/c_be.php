6f49767f69d7308cb57088629b211850935e6906
Alejandofam <za19012251@zapopan.tecmm.edu.mx>
2021-09-30 18:33:30 -0500

    agregacion del login

---
+++ loginUsuario/php/c_be.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+    //conexion rapida.
+    $conn = mysqli_connect("localhost","root","","login_register_db");
+    /*
+    if($conexion){
+        echo 'conectado exitosamente';
+
+    }else{
+        echo 'fallo la conexion';
+    }*/
+
+
+
+
+
+
---
+++ loginUsuario/php/registro_usuario_be.php
@@ -0,0 +1,19 @@
+<?php
+    //solicitamos la conexion a la base de datos.
+    include 'php/c_be.php';
+
+    $nombre_completo = $_POST['nombre_completo'];
+    $correo = $_POST['correo'];
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['contrasena'];
+
+    $query = "INSERT INTO usuarios(nombre_completo,correo,usuario,contrasena) 
+              VALUES ('$nombre_completo','$correo','$usuario','$contrasena')";//aqui estamos pasando los datos de las variables input
+
+
+$ejecutar = mysqli_query($conn, $query);
+
+
+
+
+
